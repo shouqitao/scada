1a10858a7833545643108692a47fed0ff0655e86
Tony Shou <shouqitao@hotmail.com>
2019-04-29 08:20:59 +0800

    update code

--- Report/RepBuilder/WordRepBuilder.cs
+++ Report/RepBuilder/WordRepBuilder.cs
@@ -34,43 +34,45 @@ namespace Utils.Report {
     /// </summary>
     public abstract class WordRepBuilder : RepBuilder {
         /// <summary>
-        /// Префикс XML-элемента для перехода на новую строку в WordML
+        /// WordML中换行符的XML元素的前缀
         /// </summary>
         protected const string BrPref = "w";
 
         /// <summary>
-        /// Имя XML-элемента для перехода на новую строку в WordML
+        /// WordML中换行符的XML元素的名称
         /// </summary>
         protected const string BrName = "br";
 
         /// <summary>
-        /// Имя с префиксом XML-элемента в шаблоне, значение которого может содержать директивы изменения
+        /// 名称前缀为模板的XML元素，其值可能包含更改指令
         /// </summary>
         protected const string ElemName = "w:t";
 
         /// <summary>
-        /// Высота XML-дерева от элемента строки таблицы до элемента с текстом ячейки
+        /// XML树的高度，从表行的元素到带有单元格文本的元素
         /// </summary>
         protected const int RowTreeHeight = 4;
 
 
         /// <summary>
-        /// Обрабатываемый XML-документ
+        /// 正在处理的XML文档
         /// </summary>
         protected XmlDocument xmlDoc;
 
 
+        /// <inheritdoc />
         /// <summary>
-        /// Конструктор
+        /// 构造函数
         /// </summary>
-        public WordRepBuilder()
+        protected WordRepBuilder()
             : base() {
             xmlDoc = null;
         }
 
 
+        /// <inheritdoc />
         /// <summary>
-        /// Получить формат отчёта
+        /// 获取报告格式
         /// </summary>
         public override string RepFormat {
             get { return "WordprocessingML"; }
@@ -78,19 +80,19 @@ namespace Utils.Report {
 
 
         /// <summary>
-        /// Найти атрибут (директиву) в строке и получить его значение
+        /// 在字符串中查找属性（指令）并获取其值
         /// </summary>
-        /// <param name="str">Строка для поиска</param>
-        /// <param name="attrName">Имя искомого атрибута</param>
-        /// <param name="attrVal">Значение атрибута</param>
-        /// <returns>Найден ли атрибут</returns>
+        /// <param name="str">搜索字符串</param>
+        /// <param name="attrName">要搜索的属性的名称</param>
+        /// <param name="attrVal">属性值</param>
+        /// <returns>找到属性</returns>
         protected bool FindAttr(string str, string attrName, out string attrVal) {
-            // "attrName=attrVal", вместо '=' может быть любой символ
+            // “attrName = attrVal”，而不是'='任何字符都可以
             attrVal = "";
             if (str.StartsWith(attrName)) {
                 int start = attrName.Length + 1;
                 if (start < str.Length) {
-                    int end = str.IndexOf(" ", start);
+                    int end = str.IndexOf(" ", start, StringComparison.Ordinal);
                     if (end < 0)
                         end = str.IndexOf("x", start, StringComparison.OrdinalIgnoreCase);
                     if (end < 0)
@@ -104,16 +106,16 @@ namespace Utils.Report {
         }
 
         /// <summary>
-        /// Получить XML-узлы строки таблицы и самой таблицы XML-дерева отчёта по узлу, содержащему директиву
+        /// 获取表行的XML节点以及包含该指令的节点的XML报告树的表本身
         /// </summary>
-        /// <param name="xmlNode">XML-узел, содержащий директиву</param>
-        /// <param name="rowNode">XML-узел строки таблицы отчёта</param>
-        /// <param name="tblNode">XML-узел таблицы отчёта</param>
-        /// <returns>Найдены ли XML-узлы</returns>
+        /// <param name="xmlNode">包含指令的XML节点</param>
+        /// <param name="rowNode">报表表行xml节点</param>
+        /// <param name="tblNode">报告表XML节点</param>
+        /// <returns>找到XML节点</returns>
         protected bool GetTreeNodes(XmlNode xmlNode, out XmlNode rowNode, out XmlNode tblNode) {
             try {
                 rowNode = xmlNode;
-                for (int i = 0; i < RowTreeHeight; i++)
+                for (var i = 0; i < RowTreeHeight; i++)
                     rowNode = rowNode.ParentNode;
                 tblNode = rowNode.ParentNode;
                 return true;
@@ -125,86 +127,86 @@ namespace Utils.Report {
         }
 
         /// <summary>
-        /// Установить XML-узлу текст, содержащий переносы строк, разбив при необходимости элемент на несколько
+        /// 设置包含换行符的XML节点文本，将元素分成几个
         /// </summary>
-        /// <param name="xmlNode">XML-узел</param>
-        /// <param name="text">Устанавливаемый текст</param>
-        /// <param name="textBreak">Обозначение переноса строки в устанавливаемом тексте</param>
+        /// <param name="xmlNode">XML节点</param>
+        /// <param name="text">可设置的文字</param>
+        /// <param name="textBreak">可设置文本中的换行符指定</param>
         protected void SetNodeTextWithBreak(XmlNode xmlNode, string text, string textBreak) {
-            XmlNode parNode = xmlNode.ParentNode;
+            var parNode = xmlNode.ParentNode;
             if (parNode == null)
                 throw new Exception("Parent XML element is missing.");
 
-            // отсоединить разбиваемый узел
+            // detach the node being broken
             parNode.RemoveChild(xmlNode);
-            XmlNode cloneNode = xmlNode.Clone();
+            var cloneNode = xmlNode.Clone();
 
             if (text == null) text = "";
             string uri = parNode.NamespaceURI;
             int breakLen = textBreak.Length;
 
             do {
-                // определение строки текста
-                int breakPos = text.IndexOf(textBreak);
+                // text line definition
+                int breakPos = text.IndexOf(textBreak, StringComparison.Ordinal);
                 bool haveBreak = breakPos >= 0;
                 string line = haveBreak ? text.Substring(0, breakPos) : text;
 
-                // добавление строки текста
-                XmlNode newNode = cloneNode.Clone();
+                // add text line
+                var newNode = cloneNode.Clone();
                 newNode.InnerText = line;
                 parNode.AppendChild(newNode);
 
-                // добавление тега переноса строки при необходимости
+                // add line break tag if necessary
                 if (haveBreak)
                     parNode.AppendChild(xmlDoc.CreateElement(BrPref, BrName, uri));
 
-                // обрезание обработанной части текста
+                // trimming the processed part of the text
                 text = haveBreak && breakPos + breakLen < text.Length ? text.Substring(breakPos + breakLen) : "";
             } while (text != "");
         }
 
         /// <summary>
-        /// Установить XML-узлу текст, содержащий переносы строк, разбив при необходимости элемент на несколько
+        /// Set the XML node text containing line breaks, breaking the element into several
         /// </summary>
-        /// <param name="xmlNode">XML-узел</param>
-        /// <param name="text">Устанавливаемый текст</param>
-        /// <param name="textBreak">Обозначение переноса строки в устанавливаемом тексте</param>
+        /// <param name="xmlNode">XML-taken</param>
+        /// <param name="text">Settable text</param>
+        /// <param name="textBreak">Line break designation in settable text</param>
         protected void SetNodeTextWithBreak(XmlNode xmlNode, object text, string textBreak) {
             string textStr = text == null || text.ToString() == "" ? " " : text.ToString();
             SetNodeTextWithBreak(xmlNode, textStr, textBreak);
         }
 
         /// <summary>
-        /// Установить XML-узлу текст, содержащий переносы строк "\n", разбив при необходимости элемент на несколько
+        /// Set the XML node text containing line breaks "\ n", breaking the element into several
         /// </summary>
-        /// <param name="xmlNode">XML-узел</param>
-        /// <param name="text">Устанавливаемый текст</param>
+        /// <param name="xmlNode">XML-taken</param>
+        /// <param name="text">Settable text</param>
         protected void SetNodeTextWithBreak(XmlNode xmlNode, string text) {
             SetNodeTextWithBreak(xmlNode, text, "\n");
         }
 
         /// <summary>
-        /// Установить XML-узлу текст, содержащий переносы строк "\n", разбив при необходимости элемент на несколько
+        /// Set the XML node text containing line breaks "\ n", breaking the element into several
         /// </summary>
-        /// <param name="xmlNode">XML-узел</param>
-        /// <param name="text">Устанавливаемый текст</param>
+        /// <param name="xmlNode">XML-taken</param>
+        /// <param name="text">Settable text</param>
         protected void SetNodeTextWithBreak(XmlNode xmlNode, object text) {
             SetNodeTextWithBreak(xmlNode, text, "\n");
         }
 
 
         /// <summary>
-        /// Начальная обработка дерева XML-документа
+        /// Initial processing of the XML document tree
         /// </summary>
         protected virtual void StartXmlDocProc() { }
 
         /// <summary>
-        /// Рекурсивный обход и обработка дерева XML-документа согласно директивам для получения отчёта
+        /// Recursive traversal and processing of the XML document tree according to the directives for receiving the report
         /// </summary>
-        /// <param name="xmlNode">Обрабатываемый XML-узел</param>
+        /// <param name="xmlNode">XML node being processed</param>
         protected virtual void XmlDocProc(XmlNode xmlNode) {
             if (xmlNode.Name == ElemName) {
-                // поиск директив преобразований элементов
+                // search for directives of transformations of elements
                 string nodeVal = xmlNode.InnerText;
                 string attrVal;
                 if (FindAttr(nodeVal, "repRow", out attrVal)) {
@@ -216,52 +218,53 @@ namespace Utils.Report {
                 } else if (FindAttr(nodeVal, "repVal", out attrVal))
                     ProcVal(xmlNode, attrVal);
             } else {
-                // рекурсивный перебор потомков текущего элемента
-                XmlNodeList children = xmlNode.ChildNodes;
+                // recursive enumeration of the descendants of the current element
+                var children = xmlNode.ChildNodes;
                 foreach (XmlNode node in children)
                     XmlDocProc(node);
             }
         }
 
         /// <summary>
-        /// Окончательная обработка дерева XML-документа
+        /// Final processing of the XML document tree
         /// </summary>
         protected virtual void FinalXmlDocProc() { }
 
         /// <summary>
-        /// Обработка директивы, изменяющей значение элемента
+        /// Processing a directive that changes the value of an element
         /// </summary>
-        /// <param name="xmlNode">XML-узел, содержащий директиву</param>
-        /// <param name="valName">Имя элемента, заданное директивой</param>
+        /// <param name="xmlNode">XML node containing directive</param>
+        /// <param name="valName">Element name given by directive</param>
         protected virtual void ProcVal(XmlNode xmlNode, string valName) { }
 
         /// <summary>
-        /// Обработка директивы, создающей строки таблицы
+        /// Processing directive creating table rows
         /// </summary>
-        /// <param name="xmlNode">XML-узел, содержащий директиву</param>
-        /// <param name="rowName">Имя строки, заданное директивой</param>
+        /// <param name="xmlNode">XML node containing directive</param>
+        /// <param name="rowName">The string name given by the directive</param>
         protected virtual void ProcRow(XmlNode xmlNode, string rowName) { }
 
 
+        /// <inheritdoc />
         /// <summary>
-        /// Генерировать отчёт в поток в формате WordML
+        /// Generate report to stream in WordML format
         /// </summary>
-        /// <param name="outStream">Выходной поток</param>
-        /// <param name="templateDir">Директория шаблона со '\' на конце</param>
+        /// <param name="outStream">Output stream</param>
+        /// <param name="templateDir">Template directory with '\' at the end</param>
         public override void Make(Stream outStream, string templateDir) {
-            // имя файла шаблона отчёта
+            // report template file name
             string templFileName = templateDir + TemplateFileName;
 
-            // загрузка и разбор файла шаблона в формате XML
+            // loading and parsing a template XML file
             xmlDoc = new XmlDocument();
             xmlDoc.Load(templFileName);
 
-            // создание отчёта - модификация xmlDoc
+            // report creation - modification of xmlDoc
             StartXmlDocProc();
             XmlDocProc(xmlDoc.DocumentElement);
             FinalXmlDocProc();
 
-            // запись в выходной поток
+            // write to output stream
             xmlDoc.Save(outStream);
         }
     }
